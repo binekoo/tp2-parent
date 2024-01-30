3c2eab4ae7fcf516fc728bf973173c5f253357af
binekoo <ghazal.nawel@gmail.com>
2024-01-30 17:46:51 +0100

    Ajout des tests
    dependencies : JUnit + Failsafe

---
+++ tp2-db/src/test/java/UserDAOIT.java
@@ -0,0 +1,11 @@
+package fr.upec.episen;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class UserDAOIT{
+    @Test
+    public void simpleTest(){
+        assertTrue(true);
+    }
+}
---
+++ tp2-db/src/test/java/UserDAOTest.java
@@ -0,0 +1,9 @@
+package fr.upec.episen;
+
+import static org.junit.Assert.assertTrue;
+
+public class UserDAOTest {
+    public void daoTest(){
+        assertTrue(true);
+    }
+}
